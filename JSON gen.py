11c8578c86fb457bb1ac61e2159b73af1fcdc22e
LiqidIam <mr.liqid@yandex.ru>
2026-02-02 21:53:06 +0300

    third commit test

--- JSON gen.py
+++ JSON gen.py
@@ -1,4 +1,3 @@
-#Second commit test
 import json
 
 def generate_timeline(start_year=1810, end_year=2100, step=10, width_per_year=25):
@@ -35,7 +34,6 @@ def generate_timeline(start_year=1810, end_year=2100, step=10, width_per_year=25
 	return json.dumps(canvas, indent=2, ensure_ascii=False)
 
 # Использование
-# print(generate_timeline())
 
 # Сохранить в файл .canvas
 with open("timeline.canvas", "w", encoding="utf-8") as f:
